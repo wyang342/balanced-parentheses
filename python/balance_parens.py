224b51ec1503d9dd259fc88ea5763e1c13c66434
Won Yang <won@gatech.edu>
2021-05-25 10:09:43 -0500

    adds python solution

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,28 @@
+
+
 def balance_parens(str):
-    pass
+    opening_parens_index_list = []  # keeps track of the indexes of "("
+    balanced_str_initial = ""
+
+    for i, char in enumerate(str):
+        if (char == "("):  # if char is "(", keep track of it by adding its index to list
+            opening_parens_index_list.append(i)
+        elif (char == ")"):
+            # if no opening parens tracked in list, ignore the closing parens
+            if (len(opening_parens_index_list) == 0):
+                continue
+            else:  # else remove the index of the latest tracked "(" from list
+                opening_parens_index_list.pop()
+        # adds all letters, "("'s, and matched ")"'s.
+        balanced_str_initial += char
+
+    balanced_str_final = ""
+    if (len(opening_parens_index_list) > 0):  # if there are unmatched "("'s
+        for i, char in enumerate(balanced_str_initial):
+            # add char to balanced_str_final only if it's untracked in opening_parens_index_list (which means the char is "balanced")
+            if i not in opening_parens_index_list:
+                balanced_str_final += char
+    else:  # if all chars are "balanced", add all chars to balanced_str_final
+        balanced_str_final = balanced_str_initial
+
+    return balanced_str_final
--- python/balance_parens_spec.py
+++ python/balance_parens_spec.py
@@ -1,8 +1,31 @@
+import unittest
 from balance_parens import balance_parens
 
-# Add more test cases!...
-print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
-print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
 
-# Challenge: nested parentheses...
-print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+class TestBalancedParentheses(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(balance_parens("()"), "()")
+
+    def test_case_2(self):
+        self.assertEqual(balance_parens("(a)(bdd)c)"), "(a)(bdd)c")
+
+    def test_case_3(self):
+        self.assertEqual(balance_parens("a(b)(c)())"), "a(b)(c)()")
+
+    def test_case_4(self):
+        self.assertEqual(balance_parens("((((("), "")
+
+    def test_case_5(self):
+        self.assertEqual(balance_parens("abc(d)e(fgh))(i)j)k"),
+                         "abc(d)e(fgh)(i)jk")
+
+    def test_case_6(self):
+        self.assertEqual(balance_parens("abc((d)e(fgh)(i)j(k"),
+                         "abc(d)e(fgh)(i)jk")
+
+        # # Add more test cases!...
+        # print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
+        # print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+
+        # # Challenge: nested parentheses...
+        # print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
